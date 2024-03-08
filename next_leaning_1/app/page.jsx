a3ce56c8ce88d9497c11bf58e2eeff0e360606a8
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-03-08 11:52:19 +0530

    main

--- next_leaning_1/app/page.jsx
+++ next_leaning_1/app/page.jsx
@@ -1,8 +1,10 @@
+import Page from "./doosra/Page";
 
 export default function Home() {
   return (
     <div className="text-red-300 font-extrabold">
     <h1>Learning next js</h1>
+     <Page/>
     </div>
   );
 }
